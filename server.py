cacc411fe3c3b3ade1c4d48b0db2bcdebcc8b08e
Ruthvik Vijayakumar <coderuth@outlook.com>
2018-09-17 00:37:58 +0530

    latest

--- server.py
+++ server.py
@@ -1,27 +1,51 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, flash, url_for, redirect, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 
-from werkzeug.datastructures import ImmutableMultiDict
+UPLOAD_FOLDER = 'pcap'
+ALLOWED_EXTENSIONS = set(['pcap'])
 
 app = Flask(__name__, static_url_path='/static')
-app.config['UPLOAD_FOLDER'] = '../pcap'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/process', methods = ['POST', 'GET'])
-def process():
-  # data = request.form
-  # print(data)
+def allowed_file(filename):
+  return '.' in filename and \
+    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-  file = request.form['file']
-  # print(file[0])
-  filename = secure_filename(file.filename)
-  print(filename)
-  # file.save(os.path.join(app.config['UPLOAD_FOLDER'], "input"))
-  return "OK"
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return redirect(url_for('uploaded_file', filename=filename))
+  return '''
+  <!doctype html>
+  <title>Upload new File</title>
+  <h1>Upload new File</h1>
+  <form method=post enctype=multipart/form-data>
+    <input type=file name=file>
+    <input type=submit value=Upload>
+  </form>
+  '''
 
 if __name__ == '__main__':
    app.run(debug = True)
